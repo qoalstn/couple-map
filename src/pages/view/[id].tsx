9e2011d7e44c0a30218c4456d04700f8bfd64d6e
saengki <mousson2020@gmail.com>
2023-04-14 16:56:27 +0900

    feat : 상세페이지

--- src/pages/view/[id].tsx
+++ src/pages/view/[id].tsx
@@ -1,12 +1,34 @@
+import Item from "@/component/item";
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [item, setItem] = useState({});
   console.log("id : ", id);
 
-  // return <p>Post: {id}</p>;
-  return <p>hi</p>;
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+
+  function getData() {
+    axios.get(API_URL).then((res) => {
+      console.log("data : ", res);
+      setItem(res.data);
+    });
+  }
+
+  useEffect(() => {
+    if (id) {
+      getData();
+    }
+  }, [id]);
+
+  return (
+    <div>
+      <Item item={item} />
+    </div>
+  );
 };
 
 export default Post;
